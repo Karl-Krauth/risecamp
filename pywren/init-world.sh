bb086c9036dc55d502290c735443f409d7f9f970
Karl Krauth <karl.krauth@gmail.com>
2018-10-11 20:22:59 -0700

    Updating pywren risecamp.

--- pywren/init-world.sh
+++ pywren/init-world.sh
@@ -4,8 +4,10 @@ echo "[default]" > /home/$NB_USER/.aws/credentials
 echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> /home/$NB_USER/.aws/credentials
 echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> /home/$NB_USER/.aws/credentials
 cat ~/.aws/credentials
-pywren create-config
+pywren create_config
 sed -i s/" bucket:.*"/" bucket: pywren.bucket.`echo "$AWS_ACCESS_KEY_ID" | md5sum | cut -f1 -d' '`"/ /home/$NB_USER/.pywren_config
-pywren create-bucket || echo "bucket already created"
-pywren create-role
-pywren deploy-lambda
+pywren create_bucket || echo "bucket already created"
+sed -i s/" aws_account_id:.*"/" aws_account_id: $ACCOUNT_ID"/ /home/$NB_USER/.pywren_config 
+sed -i s/" aws_lambda_role:.*"/" aws_lambda_role: lambda_role"/ /home/$NB_USER/.pywren_config
+# pywren create-role
+pywren deploy_lambda
